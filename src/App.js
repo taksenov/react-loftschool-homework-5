03967d92e14d8fe236612298c59f38b8d28aa6cc
TAksenov <taksenov@gmail.com>
2017-11-20 13:39:03 +0500

    add nav menu to App

--- src/App.js
+++ src/App.js
@@ -1,27 +1,48 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { addListener, removeListener, isAuthorized } from './AuthorizeApi';
+
 import './App.css';
-import {addListener, removeListener, isAuthorized} from './AuthorizeApi';
 
 class App extends Component {
-  state = {
-    isAuthorized
-  };
+    state = {
+        isAuthorized
+    };
 
-  componentDidMount() {
-    addListener(this.handleAuthorize);
-  }
+    componentDidMount() {
+        addListener(this.handleAuthorize);
+    }
 
-  componentWillUnmount() {
-    removeListener(this.handleAuthorize);
-  }
+    componentWillUnmount() {
+        removeListener(this.handleAuthorize);
+    }
 
-  handleAuthorize = isAuthorized => {
-    this.setState({isAuthorized});
-  };
+    handleAuthorize = isAuthorized => {
+        this.setState({ isAuthorized });
+    };
 
-  render() {
-    return null;
-  }
+    render() {
+        return (
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/auth">Войти</Link>
+                        </li>
+                        <li>
+                            <Link to="/private">Секретная страница</Link>
+                        </li>
+                        <li>
+                            <Link to="/public">Публичная страница</Link>
+                        </li>
+                        <li>
+                            <Link to="/">Главная</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        );
+    }
 }
 
 export default App;
